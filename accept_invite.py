cf27832377b780a6ecdbb23683ed1656d961118d
Ireneusz Opaliński <82705344+Irek-h@users.noreply.github.com>
2022-04-08 11:22:51 +0200

    Update accept_invite.py

--- accept_invite.py
+++ accept_invite.py
@@ -8,21 +8,21 @@ all_regions = ([i['RegionName'] for i in response['Regions']])
 
 for i in all_regions:
 	try:
-			gd = boto3.client(service_name = 'guardduty', region_name = i)
-			response = gd.list_invitations()
+		gd = boto3.client(service_name = 'guardduty', region_name = i)
+		response = gd.list_invitations()
 			
-			admin_acc_id = response['Invitations'][0]['AccountId']
-			invite_id = response['Invitations'][0]['InvitationId']
+		admin_acc_id = response['Invitations'][0]['AccountId']
+		invite_id = response['Invitations'][0]['InvitationId']
 			
-			response = gd.list_detectors()
+		response = gd.list_detectors()
 			
-			detector_id = response['DetectorIds'][0]
+		detector_id = response['DetectorIds'][0]
 			
-			response = gd.accept_invitation(
-			         DetectorId=detector_id,
-			         MasterId=admin_acc_id,
-			         InvitationId=invite_id
-			)
+		response = gd.accept_invitation(
+		         DetectorId=detector_id,
+		         MasterId=admin_acc_id,
+		         InvitationId=invite_id
+		)
 		print(response)
 	except:
 		print("Already works or an error")
